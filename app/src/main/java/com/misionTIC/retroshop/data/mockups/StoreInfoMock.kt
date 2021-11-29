8d790dffcebe8350d16958a2e9b1d5b8c479eb55
Hernan Cuy <hernan.cuy@gmail.com>
2021-11-29 18:35:01 -0500

    Working :D image mockupstoreinfo

--- app/src/main/java/com/misionTIC/retroshop/data/mockups/StoreInfoMock.kt
+++ app/src/main/java/com/misionTIC/retroshop/data/mockups/StoreInfoMock.kt
@@ -7,7 +7,7 @@ class StoreInfoMock {
         return StoreInfo(
             "1",
             "RetroShop",
-            "",
+            "https://i.postimg.cc/T1sN5Bb6/logo.png",
             "Cra. tal # tal - tal",
             "Nuestros contactos: +57 300 XXX XXXX 01 8000 23 53",
             "Nacimos en 2015 con el propósito de llevar lo que tu deseas a tu hogar"
