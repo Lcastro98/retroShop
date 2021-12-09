96be862b268debd4076bb296885ccec3324dd61b
Hernan Cuy <hernan.cuy@gmail.com>
2021-12-09 00:10:28 -0500

    Falta que traiga la longitud y latitud del firebase

--- app/src/main/java/com/misionTIC/retroshop/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/misionTIC/retroshop/ui/fragments/HomeFragment.kt
@@ -6,7 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.awaitMapLoad
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.misionTIC.retroshop.R
 import com.misionTIC.retroshop.databinding.FragmentHomeBinding
 import com.misionTIC.retroshop.ui.viewmodels.StoreViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
@@ -45,6 +55,23 @@ class HomeFragment : Fragment() {
             binding.homeContact.text = info.contact
             binding.homeDirection.text = info.address
             Glide.with(binding.root).load(info.image).into(binding.homeImage)
+
+            val mapFragment = childFragmentManager.findFragmentById(R.id.home_map) as SupportMapFragment
+            lifecycleScope.launchWhenStarted {
+                val googleMap = mapFragment.awaitMap()
+                googleMap.awaitMapLoad()
+                val latlng = LatLng(4.64749483867678, -74.10179015622796)
+                googleMap.addMarker(
+                    MarkerOptions()
+                        .position(latlng)
+                        .title(info.name!!)
+                )
+                val bounds = LatLngBounds.builder()
+                    .include(latlng)
+                    .build()
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 20))
+            }
         })
     }
+
 }
